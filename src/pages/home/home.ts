20eb2ed3e6ed6268790bd9f98621fda1e7fd5cfc
Pedro Henrique <pedrosobralxv@gmail.com>
2017-03-20 16:04:06 -0300

    feat(home): add basic html structure

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { PollService } from '../../providers/poll-service';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
-  }
-
+  constructor(public ps: PollService, public navCtrl: NavController) { }
 }
