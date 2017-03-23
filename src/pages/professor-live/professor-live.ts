688dc71274bc5f3cddcf41ba66bc1051db178892
Pedro Henrique <pedrosobralxv@gmail.com>
2017-03-23 14:55:05 -0300

    feat(live): MVP is live. Show chart in real-time

--- src/pages/professor-live/professor-live.ts
+++ src/pages/professor-live/professor-live.ts
@@ -1,8 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-import { Chart } from 'chart.js';
-import * as katex from 'katex';
+import { Chart, pattern } from 'chart.js';
+
+import {
+  PollService,
+} from '../../providers/providers';
 
 @Component({
   selector: 'page-professor-live',
@@ -12,24 +15,58 @@ export class ProfessorLivePage {
   @ViewChild('barCanvas') barCanvas;
 
   barChart: any;
+  data: any = {};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  constructor(
+    public ps: PollService,
+    public navCtrl: NavController,
+    public navParams: NavParams) { }
 
   ionViewDidLoad() {
+
+    this.ps.poll('FISC123').subscribe((poll) => {
+      if (!poll.data.length) return;
+      console.info('poll', poll);
+      this.data['questions'] = poll.data[0].questions;
+      console.info('data', this.data);
+      const labels = this.data.questions[0].options.map(x => x.text);
+      console.info('labels', labels)
+      let sum = this.data.questions[0].options.reduce((p, c) => {
+        c.votes = c.votes || 0;
+        return p + parseInt(c.votes, 10);
+      }, 0);
+
+      const data = this.data.questions[0].options.map(x => x.votes / sum);
+
+      this.initChart(labels, data);
+      this.plugin();
+    });
+  }
+
+  initChart(labels, data) {
     this.barChart = new Chart(this.barCanvas.nativeElement, {
       type: 'bar',
+      // type: 'horizontalBar',
       data: {
-        labels: ["Red", "Blue"],
+        labels,
         datasets: [{
           label: '# of Votes',
-          data: [1, 0],
+          data,
           backgroundColor: [
-            'rgba(54, 162, 235, 1.0)',
-            'rgba(54, 162, 235, 1.0)',
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 206, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(153, 102, 255)',
+            'rgb(255, 159, 64)'
           ],
           borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
           ],
           borderWidth: 1
         }]
@@ -57,21 +94,21 @@ export class ProfessorLivePage {
         }
       }
     });
-
-    this.plugin(this.barChart.chart);
   }
 
-  plugin(chart) {
+  plugin() {
     // Define a plugin to provide data labels
     Chart.plugins.register({
       afterDatasetsDraw: (chart, easing) => {
         // To only draw at the end of animation, check for easing === 1
         const ctx = chart.chart.ctx;
         chart.data.datasets.forEach((dataset, i) => {
+          //
+          // const sum = dataset.data.reduce((sum, value) => {
+          //   return sum + value;
+          // }, 0);
 
-          const sum = dataset.data.reduce((sum, value) => {
-            return sum + value;
-          }, 0);
+          // console.info('sum', sum);
 
           const meta = chart.getDatasetMeta(i);
           if (!meta.hidden) {
@@ -84,13 +121,15 @@ export class ProfessorLivePage {
               ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
 
               // Just naively convert to string for now
-              const dataString = Math.floor(dataset.data[index] / sum * 100) + '%';
+              const dataString = Math.floor(dataset.data[index] * 100) + '%';
+
+              console.info('%', dataString);
 
               // Make sure alignment settings are correct
               ctx.textAlign = 'center';
               ctx.textBaseline = 'middle';
               const position = element.tooltipPosition();
-              ctx.fillText(dataString, element._model.x, position.y - (fontSize / 2) );
+              ctx.fillText(dataString, element._model.x, position.y - (fontSize / 2));
             });
           }
         });
