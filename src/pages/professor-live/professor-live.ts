0249559b44d8fe9b09bcd2ad12c5d28425b2fb8d
Pedro Henrique <pedrosobralxv@gmail.com>
2017-03-23 11:02:12 -0300

    feat(chart): add basic static chart

--- src/pages/professor-live/professor-live.ts
+++ src/pages/professor-live/professor-live.ts
@@ -1,16 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+
+import { Chart } from 'chart.js';
+import * as katex from 'katex';
 
 @Component({
   selector: 'page-professor-live',
   templateUrl: 'professor-live.html'
 })
 export class ProfessorLivePage {
+  @ViewChild('barCanvas') barCanvas;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  barChart: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) { }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfessorLivePage');
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue"],
+        datasets: [{
+          label: '# of Votes',
+          data: [1, 0],
+          backgroundColor: [
+            'rgba(54, 162, 235, 1.0)',
+            'rgba(54, 162, 235, 1.0)',
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        tooltips: {
+          enabled: false,
+        },
+        legend: {
+          display: false,
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              max: 1,
+              beginAtZero: true
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+
+    this.plugin(this.barChart.chart);
+  }
+
+  plugin(chart) {
+    // Define a plugin to provide data labels
+    Chart.plugins.register({
+      afterDatasetsDraw: (chart, easing) => {
+        // To only draw at the end of animation, check for easing === 1
+        const ctx = chart.chart.ctx;
+        chart.data.datasets.forEach((dataset, i) => {
+
+          const sum = dataset.data.reduce((sum, value) => {
+            return sum + value;
+          }, 0);
+
+          const meta = chart.getDatasetMeta(i);
+          if (!meta.hidden) {
+            meta.data.forEach((element, index) => {
+              // Draw the text in black, with the specified font
+              ctx.fillStyle = 'rgb(0,0,0)';
+              const fontSize = element._chart.width / 17;
+              const fontStyle = 'bold';
+              const fontFamily = 'Helvetica Neue';
+              ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
+
+              // Just naively convert to string for now
+              const dataString = Math.floor(dataset.data[index] / sum * 100) + '%';
+
+              // Make sure alignment settings are correct
+              ctx.textAlign = 'center';
+              ctx.textBaseline = 'middle';
+              const position = element.tooltipPosition();
+              ctx.fillText(dataString, element._model.x, position.y - (fontSize / 2) );
+            });
+          }
+        });
+      }
+    });
   }
 
 }
