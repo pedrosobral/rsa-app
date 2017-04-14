379ed5ca35c10faeb117dbe28293792e167e58ea
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-14 17:10:43 -0300

    fix(poll): use _id

--- src/providers/poll-service.ts
+++ src/providers/poll-service.ts
@@ -39,31 +39,31 @@ export class PollService extends APIService {
   }
 
   setIsOver(poll) {
-    return this.polls.patch(poll.id, {
+    return this.polls.patch(poll._id, {
       isOver: true
     });
   }
 
   setAvailable(poll, value) {
-    return this.polls.patch(poll.id, {
+    return this.polls.patch(poll._id, {
       "available": value
     });
   }
 
   showChart(poll, questionIndex, value) {
-    return this.polls.patch(poll.id, {
+    return this.polls.patch(poll._id, {
         [`questions.${questionIndex}.showChart`]: value
     });
   }
 
   showAnswer(poll, questionIndex, value) {
-    return this.polls.patch(poll.id, {
+    return this.polls.patch(poll._id, {
         [`questions.${questionIndex}.showAnswer`]: value
     });
   }
 
   answer(poll, questionIndex, answer) {
-    return this.polls.patch(poll.id, {
+    return this.polls.patch(poll._id, {
       "$inc": {
         [`questions.${questionIndex}.options.$.votes`]: 1
       }
