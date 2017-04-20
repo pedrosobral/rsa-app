73d49c91d55b264b890009598e21af22e44ae7e8
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-20 14:01:21 -0300

    feat(poll): ask student id, return if room does not exist

--- src/pages/poll/poll.ts
+++ src/pages/poll/poll.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavParams } from 'ionic-angular';
+
+import {
+  AlertController,
+  NavController,
+  IonicPage,
+  NavParams,
+} from 'ionic-angular';
 
 import {
   FeathersProvider,
@@ -20,51 +26,119 @@ export class PollPage {
   poll: any;
   question: any;
   room: any;
+  student: String;
 
   constructor(
     public app: FeathersProvider,
     public rooms: RoomsProvider,
     public ps: PollService,
     public navParams: NavParams,
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
   ) { }
 
-  ionViewDidLoad() {
-    this.initPoll();
+  ionViewDidEnter() {
+    this.getRoomInfo();
   }
 
   getRoomInfo() {
     this.rooms.room(this.code)
       .subscribe((result) => {
-        if (!result.total) return;
+        if (!result.total) {
+          // back if room does not exist
+          this.navCtrl.pop();
+          return;
+        }
 
-        this.room = result.data[0]
-        // this.room.professor = room.user.name;
+        // get room info
+        this.room = result.data[0];
+
+        if (this.room.private) {
+          // private room
+          this.askId();
+        } else {
+          // anonymous room
+          this.app.socket().emit('anonymous enter room', { room: this.room });
+
+          // poll
+          this.initPoll();
+        }
       });
   }
 
-  ionViewDidEnter() {
-    this.app.socket().emit('enter room', this.code);
-
-    this.getRoomInfo();
-  }
-
   ionViewDidLeave() {
-    this.app.socket().emit('leave room', this.code);
+    if (this.room.private) {
+      this.app.socket().emit('student leave room');
+    } else {
+      this.app.socket().emit('anonymous leave room');
+    }
   }
 
   initPoll() {
-    this.ps.poll({ code: this.code }).subscribe((poll) => {
-      if (poll.data.length > 0 && poll.data[0].available !== -1) {
-        this.poll = poll.data[0];
-        this.question = this.poll.questions[this.poll.available];
-      } else {
-        this.poll = null;
-      }
-    });
+    this.ps.poll({ code: this.code })
+      .subscribe((poll) => {
+        if (poll.data.length > 0 && poll.data[0].available !== -1) {
+          this.poll = poll.data[0];
+          this.question = this.poll.questions[this.poll.available];
+        } else {
+          this.poll = null;
+        }
+      });
   }
 
   submit(answer) {
     this.ps.answer(this.poll, this.poll.available, answer)
       .then((res) => console.info(res));
+  }
+
+  askId() {
+    let prompt = this.alertCtrl.create({
+      enableBackdropDismiss: false,
+      title: 'Entrar',
+      message: "Essa turma precisa identificação do estudante. Por favor, entre com o seu código de identificação para continuar.",
+      inputs: [
+        {
+          name: 'id',
+          placeholder: 'Identificação'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+            this.navCtrl.pop();
+          }
+        },
+        {
+          text: 'Fazer login',
+          handler: data => {
+            // save student info
+            this.student = data.id;
+
+            // do login
+            this.doLogin(data.id);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  doLogin(id) {
+    this.rooms.login(this.room, id)
+      .then((studentOk) => {
+        if (!studentOk) {
+          return this.askId();
+        }
+
+        // emit info about student to socket
+        this.app.socket().emit('student enter room', {
+          room: this.room,
+          student: id,
+        });
+
+        // poll!
+        this.initPoll();
+      })
   }
 }
