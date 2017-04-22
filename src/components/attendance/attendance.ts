f49c56fdbdd2d471a93ed64745ec4a1808c06a05
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-22 12:07:44 -0300

    feat(attendance): input attendance

--- src/components/attendance/attendance.ts
+++ src/components/attendance/attendance.ts
@@ -1,22 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-/**
- * Generated class for the AttendanceComponent component.
- *
- * See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
- * for more info on Angular Components.
- */
 @Component({
   selector: 'attendance',
   templateUrl: 'attendance.html'
 })
 export class AttendanceComponent {
+  @Input('attendance') attendance: any;
 
-  text: string;
-
-  constructor() {
-    console.log('Hello AttendanceComponent Component');
-    this.text = 'Hello World';
-  }
+  constructor() { }
 
 }
