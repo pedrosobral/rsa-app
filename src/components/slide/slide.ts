d1ccde692ca7079c15504e69a66f07c16b80d0e1
Pedro Henrique <pedrosobralxv@gmail.com>
2017-04-24 21:10:31 -0300

    feat(slide): refactor how to show results.
    Remove chart.js

--- src/components/slide/slide.ts
+++ src/components/slide/slide.ts
@@ -1,20 +1,15 @@
 import {
   Component,
-  ViewChild,
   Input,
 } from '@angular/core';
-
-import { Chart } from 'chart.js';
 
 @Component({
   selector: 'slide',
   templateUrl: 'slide.html'
 })
 export class SlideComponent {
-  @ViewChild('canvas') canvas;
-  chart: any;
-
   labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  results = [];
 
   @Input() question: any;
   @Input() poll: any;
@@ -24,114 +19,18 @@ export class SlideComponent {
 
   ngOnChanges(changes) {
     if (changes.question) {
-      this.chartData();
+      this.dataChart();
     }
   }
 
-  chartData() {
-    const data = this.question.options.map(x => x.votes / this.question.votes);
-    const labels = this.getLabels();
+  dataChart() {
+    const calcPercentage = (votes = 0, total = 1) => votes / total;
+    const toPercentage = (number) => Math.floor(number * 100) + '%';
 
-    this.initChart(labels, data)
-    this.plugin();
-  }
-
-  getLabels() {
-    let labels = [];
-    for (let i = 0; i < this.question.options.length; i++) {
-      labels.push(this.labels[i]);
-    }
-    return labels;
-  }
-
-  initChart(labels, data) {
-    this.chart = new Chart(this.canvas.nativeElement, {
-      type: 'bar',
-      // type: 'horizontalBar',
-      data: {
-        labels,
-        datasets: [{
-          label: '# of Votes',
-          data,
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 206, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(153, 102, 255)',
-            'rgb(255, 159, 64)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        responsive: true,
-        tooltips: {
-          enabled: false,
-        },
-        legend: {
-          display: false,
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              max: 1,
-              beginAtZero: true
-            }
-          }],
-          xAxes: [{
-            ticks: {
-              beginAtZero: true,
-              fontSize: 30
-            }
-          }]
-        }
-      }
-    });
-  }
-
-  // Define a plugin to provide data labels
-  plugin() {
-    Chart.plugins.register({
-      afterDatasetsDraw: (chart, easing) => {
-        // To only draw at the end of animation, check for easing === 1
-        const ctx = chart.chart.ctx;
-        chart.data.datasets.forEach((dataset, i) => {
-
-          const meta = chart.getDatasetMeta(i);
-          if (!meta.hidden) {
-            meta.data.forEach((element, index) => {
-              // Draw the text in black, with the specified font
-              ctx.fillStyle = 'rgb(0,0,0)';
-              ctx.textAlign = 'center';
-              ctx.textBaseline = 'middle';
-
-              const fontSize = element._chart.width / 17;
-              const fontStyle = 'bold';
-              const fontFamily = 'Helvetica Neue';
-              ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
-
-              // Just naively convert to string for now
-              const dataString = Math.floor(dataset.data[index] * 100) + '%';
-
-              // Make sure alignment settings are correct
-              const position = element.tooltipPosition();
-
-              // print
-              ctx.fillText(dataString, element._model.x, position.y - (fontSize / 2));
-            });
-          }
-        });
-      }
-    });
+    const totalVotes = this.question.votes;
+    this.results = this.question.options
+      .map(option => calcPercentage(option.votes, totalVotes))
+      .map(toPercentage);
   }
 
 }
