4ad35f2e0cbc36f03e869fb3d93119fffd19c310
Pedro Henrique <pedrosobralxv@gmail.com>
2017-06-25 11:48:14 -0300

    feat(new-room): show already taken code error

--- src/pages/new-room/new-room.ts
+++ src/pages/new-room/new-room.ts
@@ -23,6 +23,8 @@ import {
 export class NewRoomPage {
   form: FormGroup;
 
+  isCodeTaken: boolean = false;
+
   constructor(
     public navParams: NavParams,
     public formBuilder: FormBuilder,
@@ -38,19 +40,22 @@ export class NewRoomPage {
     this.form = this.formBuilder.group({
       name: ['', Validators.required],
       code: ['', Validators.required],
-      default: [false, Validators.required]
+      // default: [false, Validators.required]
     });
   }
 
   submit() {
+    // clear warnings
+    this.isCodeTaken = false;
+
     // uuper case code string
     this.form.value.code = this.form.value.code.toUpperCase();
 
     this.rs.create(this.form.value)
       .then(() => this.close())
       .catch((error) => {
-        console.info('error', error);
-      })
+        this.isCodeTaken = true;
+      });
   }
 
   close() {
